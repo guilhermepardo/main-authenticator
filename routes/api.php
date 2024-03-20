dda5125d56f80f31fced250e18ac99c4055b1109
guilhermepardo <guilherme.pardoanjos@gmail.com>
2024-03-20 20:54:34 -0300

    feat: grouping user routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/user/create', 'App\Http\Controllers\User\UserController@store');
+Route::prefix('user')->group(function () {
 
+    Route::post('/create', 'App\Http\Controllers\User\UserController@store');
+
+});
