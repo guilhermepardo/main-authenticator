47456c8d50c7c31d6c16178276299ae334a02d95
guilhermepardo <guilherme.pardoanjos@gmail.com>
2024-03-20 21:24:19 -0300

    feat: replaced model data insertion by user service user creation function

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -5,47 +5,33 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
+use App\Service\User\UserService;
 
 class UserController extends Controller
 {
-    protected $model;
-    
-    public function __construct() {
-        $this->model = new User();
+    protected $service;
+
+    public function __construct()
+    {
+        $this->service = new UserService();
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         try {
-            $validator = Validator::make($request->all(), [
-                'firstName' => 'required',
-                'lastName' => 'required',
-                'username' => 'required|unique:users',
-                'email' => 'required|email|unique:users',
-                'password' => 'required',
-            ]);
-    
+            $validator = \Validator::make($request->all(), $this->service::REGISTER_REQ_PARAMS_RULES);
+
             if ($validator->fails()) {
-                return response()
-                    ->json(['errors' => $validator->errors()], Response::HTTP_BAD_REQUEST);
+                return response()->json(['errors' => $validator->errors()], Response::HTTP_BAD_REQUEST);
             }
 
-            $this->model->first_name = $request->firstName;
-            $this->model->last_name = $request->lastName;
-            $this->model->username = $request->username;
-            $this->model->email = $request->email;
-            $this->model->password = Hash::make($request->password);
-    
-            $this->model->save();
+            $this->service->createNewUser($request);
 
             return response()->json([], Response::HTTP_CREATED);
-            
+
         } catch (\Exception $e) {
             throw $e;
         }
-        
 
     }
 
