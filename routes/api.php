e86bd5b7c1b996b5a47e8ce515958ab261a93f4a
guilhermepardo <guilherme.pardoanjos@gmail.com>
2024-03-25 20:24:03 -0300

    feat: created new group auth and added login route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('user')->group(function () {
 
-    Route::post('/create', 'App\Http\Controllers\User\UserController@store');
+    Route::post('/create', 'App\Http\Controllers\User\UserController@store')->name('user.create');
+
+});
+
+Route::prefix('auth')->group(function () {
+
+    Route::post('/login', 'App\Http\Controllers\Auth\AuthController@login')->name('auth.login');
 
 });
