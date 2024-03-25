a26c3c71a318fe1414b1352c79d23671ff1edc19
guilhermepardo <guilherme.pardoanjos@gmail.com>
2024-03-25 20:25:18 -0300

    feat: created function to authenticate user with provided email and password

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $validator = \Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], Response::HTTP_BAD_REQUEST);
+        }
+        
+        $email = $request->email;
+        $password = $request->password;
+
+        $token = Auth::attempt(['email' => $email, 'password' => $password]);
+
+        if (!$token) {
+            return response()->json([], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return response()->json([
+            'accessToken' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+
 }
