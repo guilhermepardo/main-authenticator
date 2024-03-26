b3fdca055bfa5056d63fd26843fd14562afb0945
guilhermepardo <guilherme.pardoanjos@gmail.com>
2024-03-25 23:49:10 -0300

    feat: added custom handler to validate if provided token in request is valid and authorize or not request to proceed (Authenticate middleware)

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
-class Authenticate extends Middleware
+class Authenticate
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     */
-    protected function redirectTo(Request $request): ?string
+    public function handle(Request $request, \Closure $next)
     {
-        return $request->expectsJson() ? null : route('login');
+        if (!Auth::check()) {
+            $statusCode = Response::HTTP_UNAUTHORIZED;
+            $message = Response::$statusTexts[$statusCode];
+            return response()->json(['message' => $message ], $statusCode);
+        }
+        return $next($request);
     }
 }
