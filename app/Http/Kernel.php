dad836427bad6dfef9cc1b5ad7cd1639e9b299db
guilhermepardo <guilherme.pardoanjos@gmail.com>
2024-03-25 23:50:17 -0300

    feat: moved Authenticate middleware alias to route middleware (Kernel.php)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,7 +52,6 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $middlewareAliases = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
@@ -64,4 +63,9 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
+
+    protected $routeMiddleware = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
+    ];
+
 }
